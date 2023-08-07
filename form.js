fde8719d655f581716245795b90e71e42fde5db4
jensiriphone <jennetsiriphone@gmail.com>
2023-08-07 16:11:58 +1200

    changes to form

--- form.js
+++ form.js
@@ -1,8 +1,8 @@
-console.log("aaa")
+console.log()
 
 function onClick(){
     const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
 
-
-    console.log('onClick', {firstName})
+    console.log('onClick', {firstName}, {lastName})
 }
